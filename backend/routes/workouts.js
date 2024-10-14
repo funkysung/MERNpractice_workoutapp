7b7fe2ca577c76e5ad202a2ee29e3ffb9ae97da3
Sung Kim <sungkim7422@gmail.com>
2024-10-13 21:25:52 -0400

    -edited readme -added workout routing -added dummy routing

---
+++ backend/routes/workouts.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+//router function to get access to app
+const router = express.Router()
+
+//Get all workouts
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all workouts'})
+})
+
+//Get a single workout
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single workout'})
+})
+
+//Post a new workout
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new workout'})
+})
+
+//Delete a single workout
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a new workout'})
+})
+
+//Update a single workout
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a new workout'})
+})
+
+module.exports = router
+
